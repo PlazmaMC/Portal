34c8efcffba763d83eb3bcc277de51df7b455594
Alpha <dev@alpha93.kr>
2024-04-04 23:10:23 +0000

    Fix root path

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ app = FastAPI(
     default_response_class = responses.RedirectResponse
 )
 
+@app.get("/")
 @app.get("/{version:str}")
-async def main(version: str, target: int = 0):
+async def main(version: str = "", target: int = 0):
+    if version == "":
+        return "https://api.plazmamc.org/v1/download"
     return f"https://api.plazmamc.org/v1/download/{version}?target={target}"
