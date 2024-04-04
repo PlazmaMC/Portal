7e0058114074344e23bad4435313926b28ef53b6
Alpha <dev@alpha93.kr>
2024-04-04 22:59:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import *
+from fastapi.responses import RedirectResponse
+
+app = FastAPI(
+    docs_url = None,
+    redoc_url = None,
+    default_response_class = responses.RedirectResponse
+)
+
+@app.get("/", status_code = 308)
+@app.get("/{version:str}", status_code = 308)
+async def main(version: str = None, redirect: bool = False):
+    if redirect:
+        return f"https://api.plazmamc.org/v1/builds/{version}"
+
+    return f"https://api.plazmamc.org/v1/build/{version}"
